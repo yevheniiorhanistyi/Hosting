47440a3248f46f5988336cc7f8511d44951094b4
Jekson <organistiy@gmail.com>
2022-03-30 12:26:52 +0200

    testimonials

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper('.swiper', {
+    loop: true,
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.btn-testimonials--next',
+        prevEl: '.btn-testimonials--prev',
+    },
+
+});
