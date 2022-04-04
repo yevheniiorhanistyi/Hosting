2261a6e362c736067438c7608811bf5c870c1bb0
Jekson <organistiy@gmail.com>
2022-04-04 20:22:06 +0200

    mobile

--- js/script.js
+++ js/script.js
@@ -8,3 +8,59 @@ const swiper = new Swiper('.swiper', {
     },
 
 });
+
+// document.querySelector('.btn-burger').addEventListener('click', (event) => {
+//     document.querySelector('.btn-burger').classList.toggle('btn-burger--active');
+//     document.querySelector('.btn__nav').classList.toggle('btn__nav--active');
+//     document.querySelector('.nav').classList.toggle('nav--active');
+//     const header = document.querySelector('.header');
+
+//     header.classList.toggle('header--active');
+
+//     if (header.classList.contains('header--active')) {
+//         hideScroll();
+//     }
+//     else {
+//         showScroll();
+//     }
+// });
+
+// const hideScroll = () => {
+//     const scrollWidth = `${getScrollbarWidth()}px`;
+
+//     document.body.style.paddingRight = scrollWidth;
+//     document.body.style.overflow = 'hidden';
+// };
+// const showScroll = () => {
+//     document.body.style.paddingRight = '';
+//     document.body.style.overflow = 'visible';
+// };
+
+// const resetNav = () => {
+//     document.querySelector('.header').classList.remove('header--active');
+//     document.querySelector('.btn-burger').classList.remove('btn-burger--active');
+//     document.querySelector('.btn__nav').classList.remove('btn__nav--active');
+//     document.querySelector('.nav').classList.remove('nav--active');
+//     showScroll();
+// };
+
+// window.addEventListener('resize', resetNav);
+
+// // Get scrollbar width
+// const getScrollbarWidth = () => {
+
+//     const outer = document.createElement('div');
+
+//     outer.style.position = 'absolute';
+//     outer.style.top = '-9999px';
+//     outer.style.width = '50px';
+//     outer.style.height = '50px';
+//     outer.style.overflow = 'scroll';
+//     outer.style.visibility = 'hidden';
+
+//     document.body.appendChild(outer);
+//     const scrollbarWidth = outer.offsetWidth - outer.clientWidth;
+//     document.body.removeChild(outer);
+
+//     return scrollbarWidth;
+// };
