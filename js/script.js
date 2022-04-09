5964b977fa69b92635cded941d14a10eb80e4646
Jekson <organistiy@gmail.com>
2022-04-09 17:04:29 +0200

    burger

--- js/script.js
+++ js/script.js
@@ -9,58 +9,82 @@ const swiper = new Swiper('.swiper', {
 
 });
 
-// document.querySelector('.btn-burger').addEventListener('click', (event) => {
-//     document.querySelector('.btn-burger').classList.toggle('btn-burger--active');
-//     document.querySelector('.btn__nav').classList.toggle('btn__nav--active');
-//     document.querySelector('.nav').classList.toggle('nav--active');
-//     const header = document.querySelector('.header');
+window.addEventListener('scroll', trackScroll, { passive: true });
+let pageHeight = document.documentElement.scrollHeight;
+const navItems = document.querySelectorAll('.nav__item');
 
-//     header.classList.toggle('header--active');
+function trackScroll() {
+    let scrolled = window.pageYOffset;
+    let deviceWidth = window.outerWidth;
+    let scrolledPercent = Math.round((scrolled * 100) / pageHeight);
+    let btnBurger = document.querySelector('.btn-burger');
 
-//     if (header.classList.contains('header--active')) {
-//         hideScroll();
-//     }
-//     else {
-//         showScroll();
-//     }
-// });
+    if (scrolledPercent > 2 && deviceWidth <= 780) {
+        btnBurger.classList.add('btn-burger-show');
+    }
+    else {
+        btnBurger.classList.remove('btn-burger-show');
+    }
 
-// const hideScroll = () => {
-//     const scrollWidth = `${getScrollbarWidth()}px`;
+}
 
-//     document.body.style.paddingRight = scrollWidth;
-//     document.body.style.overflow = 'hidden';
-// };
-// const showScroll = () => {
-//     document.body.style.paddingRight = '';
-//     document.body.style.overflow = 'visible';
-// };
+document.querySelector('.btn-burger').addEventListener('click', (event) => {
+    document.querySelector('.btn-burger').classList.toggle('btn-burger--active');
+    document.querySelector('.nav').classList.toggle('nav--active');
+    const header = document.querySelector('.header');
 
-// const resetNav = () => {
-//     document.querySelector('.header').classList.remove('header--active');
-//     document.querySelector('.btn-burger').classList.remove('btn-burger--active');
-//     document.querySelector('.btn__nav').classList.remove('btn__nav--active');
-//     document.querySelector('.nav').classList.remove('nav--active');
-//     showScroll();
-// };
+    for (let i = 0; i < navItems.length; i++) {
+        navItems[i].classList.add('nav__item--active');
 
-// window.addEventListener('resize', resetNav);
+        navItems[i].addEventListener('click', resetNav);
+    }
 
-// // Get scrollbar width
-// const getScrollbarWidth = () => {
+    if (document.querySelector('.btn-burger').classList.contains('btn-burger--active')) {
+        hideScroll();
+    }
+    else {
+        showScroll();
+    }
+});
 
-//     const outer = document.createElement('div');
+const hideScroll = () => {
+    const scrollWidth = `${getScrollbarWidth()}px`;
 
-//     outer.style.position = 'absolute';
-//     outer.style.top = '-9999px';
-//     outer.style.width = '50px';
-//     outer.style.height = '50px';
-//     outer.style.overflow = 'scroll';
-//     outer.style.visibility = 'hidden';
+    document.body.style.paddingRight = scrollWidth;
+    document.body.style.overflow = 'hidden';
+};
+const showScroll = () => {
+    document.body.style.paddingRight = '';
+    document.body.style.overflow = 'visible';
+};
 
-//     document.body.appendChild(outer);
-//     const scrollbarWidth = outer.offsetWidth - outer.clientWidth;
-//     document.body.removeChild(outer);
+const resetNav = () => {
+    document.querySelector('.btn-burger').classList.remove('btn-burger--active');
+    document.querySelector('.btn__nav').classList.remove('btn__nav--active');
+    document.querySelector('.nav').classList.remove('nav--active');
+    for (let i = 0; i < navItems.length; i++) {
+        navItems[i].classList.remove('nav__item--active');
+    }
+    showScroll();
+};
 
-//     return scrollbarWidth;
-// };
+window.addEventListener('resize', resetNav);
+
+// Get scrollbar width
+const getScrollbarWidth = () => {
+
+    const outer = document.createElement('div');
+
+    outer.style.position = 'absolute';
+    outer.style.top = '-9999px';
+    outer.style.width = '50px';
+    outer.style.height = '50px';
+    outer.style.overflow = 'scroll';
+    outer.style.visibility = 'hidden';
+
+    document.body.appendChild(outer);
+    const scrollbarWidth = outer.offsetWidth - outer.clientWidth;
+    document.body.removeChild(outer);
+
+    return scrollbarWidth;
+};
